f4a6ccf8ec339f61233af51a613672757b8f0959
Mark Wong <markmcwong@yahoo.com>
2021-05-06 06:16:14 +0800

    Rename working folder

--- projects/skilio-todos/src/environments/environment.prod.ts
+++ projects/skilio-todos/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   envName: 'PROD',
   production: true,
   test: false,
-  i18nPrefix: '/angular-ngrx-material-starter',
+  i18nPrefix: '/skilio-todos',
   versions: {
     app: packageJson.version,
     angular: packageJson.dependencies['@angular/core'],
